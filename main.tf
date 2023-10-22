f5a7bd476eea06f69e85fa809ceb3ef3a412b4c1
Chinedu <obichinedu@gmail.com>
2023-10-22 17:12:40 +0000

    #42 :fire: Implementing CRUD (CREATE, READ, UPDATE and DELETE)

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -36,3 +36,14 @@ provider "terratowns" {
 #  error_html_filepath = var.error_html_filepath
 #  content_version = var.content_version
 #}
+
+resource "terratowns_home" "home" {
+  name = "How to play Blue sky in 2023!"
+  description = <<DESCRIPTION
+Blue sky is a building cast for developers
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
